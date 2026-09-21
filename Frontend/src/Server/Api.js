234797440eb55466a246433f582e0bffa45243ef
shivashankar9346 <kotteshivashankar9346@gmail.com>
2026-09-21 12:51:26 +0530

    Change API base URL to production server

--- Frontend/src/Server/Api.js
+++ Frontend/src/Server/Api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL :"http://localhost:3000"
+    baseURL :"https://swapzone-lx5g.onrender.com"
 })
 
 export async function register( form) {
