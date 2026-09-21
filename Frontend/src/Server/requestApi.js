d459fa09c1adccfb3358d0d5524fcbbe5ed09674
shivashankar9346 <kotteshivashankar9346@gmail.com>
2026-09-21 12:51:45 +0530

    Change API base URL to production server

--- Frontend/src/Server/requestApi.js
+++ Frontend/src/Server/requestApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://swapzone-lx5g.onrender.com"
 });
 
 // Get token from localStorage
