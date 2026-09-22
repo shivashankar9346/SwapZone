05da8a41e30c4e3e8760e85c190af8c544b2f3c4
shivashankar9346 <kotteshivashankar9346@gmail.com>
2026-09-22 06:58:05 +0530

    Add CORS origin for production server

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 3000;
 connectDB()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    "https://swapzone-1-r4yb.onrender.com"
 }));
 app.use(express.json());
 
