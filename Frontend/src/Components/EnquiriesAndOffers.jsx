01a8ee66def1dd0024b9eb4135688a4d5abfb286
shiva <kotteshivashankar9346@gmail.com>
2026-09-22 11:01:29 +0530

    cors updated

--- Frontend/src/Components/EnquiriesAndOffers.jsx
+++ Frontend/src/Components/EnquiriesAndOffers.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./EnquiriesAndOffers.css";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://swapzone-t5nu.onrender.com";
 
 const EnquiriesAndOffers = () => {
 
