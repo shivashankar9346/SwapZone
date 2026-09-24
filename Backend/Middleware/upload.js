b03a3f12a527e30700106f15a066cf64d5086820
shiva <kotteshivashankar9346@gmail.com>
2026-09-24 18:50:23 +0530

    uploads file updated

--- Backend/Middleware/upload.js
+++ Backend/Middleware/upload.js
@@ -1,50 +1,15 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({
-
-    destination: (req, file, cb) => {
-        cb(null, "uploads/");
-    },
-
-    filename: (req, file, cb) => {
-
-        const uniqueName =
-            Date.now() + "-" + file.originalname;
-
-        cb(null, uniqueName);
-    }
-
-});
-
-const upload = multer({
-    storage: storage
-});
-
-export default upload;
-
-
-
 // import multer from "multer";
-// import fs from "fs";
-// import path from "path";
-
-// const uploadDir = path.join(process.cwd(), "uploads");
-
-// // Create uploads folder if it doesn't exist
-// if (!fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir, { recursive: true });
-// }
 
 // const storage = multer.diskStorage({
 
 //     destination: (req, file, cb) => {
-//         cb(null, uploadDir);
+//         cb(null, "uploads/");
 //     },
 
 //     filename: (req, file, cb) => {
 
 //         const uniqueName =
-//             `${Date.now()}-${file.originalname}`;
+//             Date.now() + "-" + file.originalname;
 
 //         cb(null, uniqueName);
 //     }
@@ -56,3 +21,38 @@ export default upload;
 // });
 
 // export default upload;
+
+
+
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+
+    filename: (req, file, cb) => {
+
+        const uniqueName =
+            `${Date.now()}-${file.originalname}`;
+
+        cb(null, uniqueName);
+    }
+
+});
+
+const upload = multer({
+    storage: storage
+});
+
+export default upload;
