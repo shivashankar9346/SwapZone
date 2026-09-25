4a3ebb4d993f5816024e0675345ab6ac175857ed
shiva <kotteshivashankar9346@gmail.com>
2026-09-25 12:12:46 +0530

    upadted

--- Backend/DataBase/cloudinary.js
+++ Backend/DataBase/cloudinary.js
@@ -3,8 +3,13 @@ import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+console.log("☁️ CLOUDINARY CONFIG CHECK:", {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key_exists: !!process.env.CLOUDINARY_API_KEY,
+    api_secret_exists: !!process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
